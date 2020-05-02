8ad64bab9a37b6bf74743de99b83478bbbfb4efc
glaphire <motoryna.dariia@pdffiller.team>
2020-05-02 17:30:16 +0300

    tutorial 4. Routing

--- src/receive_log_direct.php
+++ src/receive_log_direct.php
@@ -6,16 +6,24 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->exchange_declare('logs', 'fanout', false, false, false);
+$channel->exchange_declare('direct_logs', 'direct', false, false, false);
 
 list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
 
-$channel->queue_bind($queue_name, 'logs');
+$severities = array_slice($argv, 1);
 
-echo " [*] Waiting for logs. To exit press CTRL+C\n";
+if (empty($severities)) {
+    file_put_contents('php://strerr', "Usage: $argv[0] [info] [warning] [error]\n");
+}
+
+foreach ($severities as $severity) {
+    $channel->queue_bind($queue_name, 'direct_logs', $severity);
+}
+
+echo "[*] Waiting for logs. To exit press CRTL+C\n";
 
 $callback = function ($msg) {
-    echo ' [x] ', $msg->body, "\n";
+    echo ' [x] ' . $msg->delivery_info['routing_key'] . ': ' . $msg->body . PHP_EOL;
 };
 
 $channel->basic_consume($queue_name, '', false, true, false, false, $callback);
